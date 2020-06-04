3d862088ec3daaa2df627f9d277a804a011bce6a
Danilo Andrade Peixoto Rêgo <danilo.rego@cubos.io>
2020-06-04 17:36:19 -0300

    feat: loading list

--- lib/scenes/list_example/list_example_screen.dart
+++ lib/scenes/list_example/list_example_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:statemanagementsclass/scenes/components/empty_list_screen.dart';
+import 'package:statemanagementsclass/scenes/components/loading_list_screen.dart';
 
 class ListExampleScreen extends StatefulWidget {
   @override
